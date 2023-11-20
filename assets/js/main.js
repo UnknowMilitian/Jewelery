5330ab84aeaf4f34888db4bed39d80e421b07694
UnknowMilitiant <j.umariy4238@gmail.com>
2023-11-20 19:19:37 +0500

    Project Completed 😤✊ (if I don't find any bugs in the future)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,22 @@
 // Navbar
-let navbarBar = document.querySelector(".bar");
 let mobileNavbar = document.querySelector(".mobile-navbar");
-let mobileLi = document.querySelectorAll(".navbar-mobile-link");
+const toggleMobileNavbar = () => mobileNavbar.classList.toggle("show");
+const closeMobileNavbar = () => mobileNavbar.classList.remove("show");
 
-navbarBar.addEventListener("click", () => {
-  mobileNavbar.classList.toggle("show");
+document.querySelector(".bar").addEventListener("click", toggleMobileNavbar);
+document.querySelectorAll(".navbar-mobile-link").forEach((element) => {
+  element.addEventListener("click", closeMobileNavbar);
 });
 
-mobileLi.forEach((element) => {
-  element.addEventListener("click", () => {
+window.addEventListener("click", (event) => {
+  if (!event.target.closest("nav")) {
     mobileNavbar.classList.remove("show");
-  });
+  }
 });
 // /Navbar
 
 // Splide
-let splide = new Splide(".splide", {
+const splide = new Splide(".splide", {
   type: "loop",
   perPage: 4,
   gap: "5rem",
@@ -24,18 +25,56 @@ let splide = new Splide(".splide", {
   arrows: false,
   pagination: false,
   autoplay: true,
-
   breakpoints: {
-    1000: {
-      perPage: 3,
-      gap: "4rem",
-    },
-
-    800: {
-      perPage: 2,
-    },
+    1000: { perPage: 3, gap: "4rem" },
+    800: { perPage: 2 },
   },
 });
-
 splide.mount();
 // /Splide
+
+// IntersectionObserver
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("element-show");
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5],
+};
+
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
+// /IntersectionObserver
+
+// Filer
+document.querySelectorAll(".product").forEach((product) => {
+  const prices = product.querySelectorAll(".price:not(.default)");
+  const buttons = product.querySelectorAll(".filter-btn:not(.default)");
+  const defaultPrice = product.querySelector(".price.default");
+
+  defaultPrice.classList.add("active");
+  prices.forEach((price) => (price.style.display = "none"));
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const size = button.getAttribute("data-size");
+      const priceToShow = product.querySelector(`.price[data-size="${size}"]`);
+
+      defaultPrice.classList.remove("active");
+      defaultPrice.style.display = "none";
+      prices.forEach((price) => (price.style.display = "none"));
+
+      buttons.forEach((btn) => btn.classList.toggle("active", btn === button));
+      priceToShow && (priceToShow.style.display = "block");
+    });
+  });
+});
+// /Filer
